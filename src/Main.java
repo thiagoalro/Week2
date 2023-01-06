62e4c81a1a13861818642de5e0b95d6dbb2acfa8
thiago <thiago.alves@ifce.edu.br>
2023-01-06 12:17:24 -0300

    get value of a variable from an user

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("Hello world!");
 		double preco = 20.0;
-		System.out.printf("preco: %f%n", preco);
+		System.out.printf("preco: %.2f%n", preco);
 		preco = preco + 0.90;
-		System.out.printf("preco: %f%n", preco);
+		System.out.printf("preco: %.2f%n", preco);
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Digite o primeiro número: ");
+		int number1 = input.nextInt();
+		System.out.print("Digite o segundo número: ");
+		int number2 = input.nextInt();
+		int sum = number1 + number2;
+		System.out.printf("Soma: %d%n", sum);
 
 	}
 }
