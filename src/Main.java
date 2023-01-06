62078707783ea93939c955a17cef7566d97d288c
thiago <thiago.alves@ifce.edu.br>
2023-01-06 12:11:44 -0300

    change the value of a variable

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+
+		System.out.println("Hello world!");
+		double preco = 20.0;
+		System.out.printf("preco: %f%n", preco);
+		preco = preco + 0.90;
+		System.out.printf("preco: %f%n", preco);
+
+	}
+}
